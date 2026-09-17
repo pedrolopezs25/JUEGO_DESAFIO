6dd860047c32020ddf6ea225959b90a0c558c99c
pedrol <pedro.lopezs@udea.edu.co>
2026-09-17 11:14:47 -0500

    eliminar filas

--- SWEET_RUSH/main.cpp
+++ SWEET_RUSH/main.cpp
@@ -3,21 +3,67 @@
 #include "funciones.h"
 using namespace std;
 
-// void eliminarFichaYAplicarGravedad(unsigned char* tablero,int filas,int columnas,int fila,int columna) {
-//     eliminarFicha(tablero, filas, columnas, fila, columna);
-//     int fichas, cascadas = procesarCombinaciones(tablero, filas, columnas);
-//     cout << "cascadas completadas: " << cascadas << " fichas eliminadas:  " << fichas << endl;
-//     aplicarGravedadEnColumna(tablero, filas, columnas);
-// }
-// MAIN
-//  MAIN
-// MAIN
+
+void agregarFila(unsigned char*& tablero, int& filas, int columnas, int posicionNuevaFila) {
+    // Se puede insertar desde la fila 0 hasta después de la última fila.
+    if (posicionNuevaFila < 0 || posicionNuevaFila > filas) {
+        cout << "usted está agregando en una posicion que no existe" << endl;
+        return;
+    }
+
+    int filasNuevas = filas + 1;
+
+    int bytesNuevos = calcularCantidadBytes(filasNuevas, columnas);
+    unsigned char* tableroNuevo = new unsigned char[bytesNuevos];
+
+    // Inicializar todos los bytes para evitar bits con basura.
+    for (int i = 0; i < bytesNuevos; i++) {
+        tableroNuevo[i] = 0;
+    }
+
+    // Copiar las filas originales antes de la nueva fila.
+    for (int fila = 0; fila < posicionNuevaFila; fila++) {
+        for (int columna = 0; columna < columnas; columna++) {
+            int posicionVieja = calcularPosicionLogica(fila, columna, columnas);
+            int posicionNueva = calcularPosicionLogica(fila, columna, columnas);
+            unsigned char ficha = leerFicha(tablero, posicionVieja);
+            escribirFicha(tableroNuevo, posicionNueva, ficha);
+        }
+    }
+
+    // Llenar la fila insertada con fichas aleatorias.
+    for (int columna = 0; columna < columnas; columna++) {
+        int posicionNueva = calcularPosicionLogica(posicionNuevaFila, columna, columnas);
+        unsigned char ficha = rand() % 6;
+        escribirFicha(tableroNuevo, posicionNueva, ficha);
+    }
+
+    // Copiar las filas originales desde la posición de inserción.
+    // Su destino se desplaza una fila hacia abajo.
+    for (int fila = posicionNuevaFila; fila < filas; fila++) {
+        for (int columna = 0; columna < columnas; columna++) {
+            int posicionVieja = calcularPosicionLogica(fila, columna, columnas);
+            int posicionNueva = calcularPosicionLogica(fila + 1, columna, columnas);
+            unsigned char ficha = leerFicha(tablero, posicionVieja);
+            escribirFicha(tableroNuevo, posicionNueva, ficha);
+        }
+    }
+    delete[] tablero;
+    tablero = tableroNuevo;
+    filas = filasNuevas;
+}
+
+//-------------------------------
+//-----------  MAIN  ------------
+//-------------------------------
 int main() {
     srand(time(nullptr));
-    unsigned char* tablero = new unsigned char[4];
 
     int filas = 6;
     int columnas = 10;
+    int cantidaddebytes = calcularCantidadBytes(filas, columnas);
+    unsigned char* tablero = new unsigned char[cantidaddebytes];
+
     //bool* marcadores = new bool[filas * columnas];
 
 //##################################################################
@@ -46,7 +92,11 @@ int main() {
 
     cout << "Tablero despues de eliminar:" << endl;
     mostrarTableroNumerico(tablero, filas, columnas);
-
+    int filaNueva;
+    cout << "Ingrese la fila a agregar" << endl;
+    cin >> filaNueva;
+    agregarFila(tablero,filas,columnas, filaNueva);
+    mostrarTableroNumerico(tablero, filas, columnas);
     delete[] tablero;
 
 
